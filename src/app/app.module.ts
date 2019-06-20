458556c6ab460f1c9aef74813307091476720fce
Kelvin Lui <kelvin97lui@gmail.com>
2019-06-20 13:58:34 -0400

    add admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CustomerComponent } from './components/customer/customer.component';
 import { CustomerProfileComponent } from './components/customer/customer-profile/customer-profile.component';
 import { VendorComponent } from './components/vendor/vendor.component';
 import { VendorProfileComponent } from './components/vendor/vendor-profile/vendor-profile.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { VendorProfileComponent } from './components/vendor/vendor-profile/vendo
     CustomerComponent,
     CustomerProfileComponent,
     VendorComponent,
-    VendorProfileComponent
+    VendorProfileComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
