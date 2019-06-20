e64ce8aa491ed8288da4e5d5d281a3f86655f6dc
Kelvin Lui <kelvin97lui@gmail.com>
2019-06-20 13:50:25 -0400

    add customer-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { CustomerProfileComponent } from './components/customer-profile/customer-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    CustomerProfileComponent
   ],
   imports: [
     BrowserModule,
