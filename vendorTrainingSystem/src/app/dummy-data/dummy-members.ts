0693cb2d81bacc2e51ae8aab68486f76af7b84a0
Kelvin Lui <kelvin97lui@gmail.com>
2019-06-20 20:03:46 -0400

    add dummy members data

---
+++ vendorTrainingSystem/src/app/dummy-data/dummy-members.ts
@@ -0,0 +1,20 @@
+import { Member } from '../models/Member';
+
+export const dummy_members:Member[] = [
+  {
+    _id:601,
+    email: "bobdylan@gmail.com",
+    first_name: "Bob",
+    last_name: "Dylan",
+    role_id: 0,
+    phone: "1-347-1234",
+    address: "123 Baker Street",
+    public: true,
+    password: "abc123",
+    v_id: 100,
+    companyRole: "Head of Lettuce Quality Assurance"
+  }
+];
+
+
+    
