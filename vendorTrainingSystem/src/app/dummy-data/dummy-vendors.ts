a0d682c45a88c1f04bef0a3c06e29b2fe7b5e1f6
Kelvin Lui <kelvin97lui@gmail.com>
2019-06-20 20:03:17 -0400

    add dummy vendors data

---
+++ vendorTrainingSystem/src/app/dummy-data/dummy-vendors.ts
@@ -0,0 +1,27 @@
+import { Vendor } from '../models/Vendor';
+
+export const dummy_vendors:Vendor[] = [
+  {
+    _id: 100,
+    name: "Spring Vendor Services",
+    address: "123 York Ave.",
+    phone: "1-800-1234",
+    email: "spring123@gmail.com"
+  },
+  {
+    _id: 200,
+    name: "Fall Vendor Services",
+    address: "456 Nature Ave.",
+    phone: "1-800-4567",
+    email: "nature456@gmail.com"
+  },
+  {
+    _id: 300,
+    name: "Winter Vendor Services",
+    address: "123 Epson Ave.",
+    phone: "1-800-8976",
+    email: "winter8976@gmail.com"
+  }
+];
+
+
