b532266e20cc432a7e76760d00bf113b6d07c65d
David Dataram <davedataram@gmail.com>
2019-06-29 23:04:31 -0400

    Added Djamgo rest framework to project

--- vendortraining/settings.py
+++ vendortraining/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mysql'
+    'django_mysql',
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vendortraining.wsgi.application'
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
