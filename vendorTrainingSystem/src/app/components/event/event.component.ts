eaa336265871f6e3026186a2be39dcf20a26c56c
edwardcerverizzo1 <Edward.Cerverizzo@gmail.com>
2019-07-09 13:19:12 -0400

    made list-events pretty

--- vendorTrainingSystem/src/app/components/event/event.component.ts
+++ vendorTrainingSystem/src/app/components/event/event.component.ts
@@ -5,7 +5,8 @@ import { Event } from '../../models/Event';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.css'],
+  host:{"class" : "col-12 col-md-3"}
 })
 export class EventComponent implements OnInit {
 
