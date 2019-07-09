4629d78bd52ff05265ce5b5a0a18e4feafea4d78
Karanvir Singh <ksin3798@gmail.com>
2019-07-09 13:53:50 -0400

    changed into div tags and added classes

--- vendorTrainingSystem/src/app/modals/register.component.ts
+++ vendorTrainingSystem/src/app/modals/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
+    selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
