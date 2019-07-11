7caa1bb204387c1569abf0667ab106559ba76ea9
Karanvir Singh <ksin3798@gmail.com>
2019-07-11 14:24:10 -0400

    creating modal functionality in progress

--- vendorTrainingSystem/src/app/modals/login/login.component.ts
+++ vendorTrainingSystem/src/app/modals/login/login.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   email1: string;
-  password1: string
+  password1: string;
 
   ngOnInit() {
   }
