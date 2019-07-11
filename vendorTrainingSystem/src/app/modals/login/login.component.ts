a26db3b14f6d21f97d6fd9d907a73e5ecff5c0b6
Karanvir Singh <ksin3798@gmail.com>
2019-07-11 12:47:27 -0400

    finished validation for login

--- vendorTrainingSystem/src/app/modals/login/login.component.ts
+++ vendorTrainingSystem/src/app/modals/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment'
+
+interface ContactInfoModel{
+  password1: string;
+  email: string;
+}
 
 @Component({
   selector: 'dsol-login',
@@ -6,20 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  contactModel = <ContactInfoModel>{}
   email1: string;
   password1: string;
   //for error messaging 
   errorMsgEmail: "";
-  errorMsgPassword: "";
+  errorMsgPassword: ""; 
+  emailRegex = environment.regExTests.emailRegex;
+  phoneRegex = environment.regExTests.phoneRegex;
 
-  constructor() { }
+  constructor() { 
+    this.contactModel.password1="abc123!";
+    this.contactModel.email="ContactGoogle@gmail.com";
+  }
 
   ngOnInit() {
-    /*these are temporary 
-  submitted = false;
+  }
 
-  ngSubmit() { this.submitted = true; }
-  */
+  editConfirm(){
+    let valid: boolean = true;
+    
+    if(!environment.regExTests.emailRegex.test(this.contactModel.email)){
+      
+      valid = false;
+    }
+    if(!environment.regExTests.phoneRegex.test(this.contactModel.email)){
+      
+      valid = false;
+    }
+    if(this.contactModel.password1 === ""){
+      
+      valid = false;
+    }
+    
+    
   }
 
 }
