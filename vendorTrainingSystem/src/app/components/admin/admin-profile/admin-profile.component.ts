d2bc6627e3ec4b7f12e792399f00d407df87edf5
czcz7890 <dongsoochung99@gmail.com>
2019-07-11 14:07:25 -0400

    basic display for admin profile

--- vendorTrainingSystem/src/app/components/admin/admin-profile/admin-profile.component.ts
+++ vendorTrainingSystem/src/app/components/admin/admin-profile/admin-profile.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface AdminProfileModel{
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  password: string
+}
+
 @Component({
   selector: 'app-admin-profile',
   templateUrl: './admin-profile.component.html',
   styleUrls: ['./admin-profile.component.css']
 })
 export class AdminProfileComponent implements OnInit {
-
-  constructor() { }
-
+  adminProfileModel = <AdminProfileModel>{};
+  Editing: boolean = false;
+  constructor() {
+    this.adminProfileModel.firstName = "John";
+    this.adminProfileModel.lastName = "Smith";
+    this.adminProfileModel.email = "sdsds@gmail.com";
+    this.adminProfileModel.phoneNumber = "123-456-7890";
+    this.adminProfileModel.password = "hello123";
+  }
+  
   ngOnInit() {
   }
 
