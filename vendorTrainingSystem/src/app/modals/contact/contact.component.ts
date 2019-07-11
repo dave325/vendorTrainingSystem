7e35fbd1fc4d49fcbd54219f5085021ab7437fbb
czcz7890 <dongsoochung99@gmail.com>
2019-07-10 21:50:03 -0400

    finished initial design of contact info. up for code review

--- vendorTrainingSystem/src/app/modals/contact/contact.component.ts
+++ vendorTrainingSystem/src/app/modals/contact/contact.component.ts
@@ -7,28 +7,59 @@ import {globals} from '../../globals'
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  name: String;
-  address: String;
-  phoneNumber: String;
-  email: String;
-  editing: Boolean;
+  name: string;
+  phoneNumber: string;
+  email: string;
+  editing: boolean;
+  //probably temporary solution to error messaging
+  errorMsgName: string; 
+  errorMsgPhoneNumber: string;
+  errorMsgEmail: string;
+
   constructor() {
     this.editing = false;
     this.name="Google LLC";
-    this.address="1600 Amphitheatre Parkway, Mountain View, CA 94043, USA";
-    this.phoneNumber="123-503-4867",
-    this.email="ContactGoogle@gmail.com"
+    this.phoneNumber="123-503-4867";
+    this.email="ContactGoogle@gmail.com";
+    //error messaging
+    this.errorMsgName = "";
+    this.errorMsgPhoneNumber = "";
+    this.errorMsgEmail = "";
+
    }
 
   ngOnInit() {
+    
   }
 
   editConfirm(){
-    if(!globals.regExTests.emailRegex.test(name)){
+    let valid: boolean;
+    valid = true;
 
+    if(!globals.regExTests.emailRegex.test(this.email)){
+      this.errorMsgEmail = "Invalid Email Address";
+      valid = false;
+    }else{
+      this.errorMsgEmail = "";
     }
-    this.editing=false;
-    //ask for confirmation from database
+    if(!globals.regExTests.phoneRegex.test(this.phoneNumber)){
+      this.errorMsgPhoneNumber="Invalid Phone Number";
+      valid = false;
+    }else{
+      this.errorMsgPhoneNumber = "";
+    }
+    if(this.name === ""){
+      this.errorMsgName = "Name field cannot be empty";
+      valid = false;
+    }else{
+      this.errorMsgName = "";
+    }
+    
+    if(valid){
+      this.editing=false;
+      //ask for confirmation from database
+    }
+    
   }
 
 }
