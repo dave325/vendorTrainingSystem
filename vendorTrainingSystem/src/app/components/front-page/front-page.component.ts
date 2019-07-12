48dba829af57ceb52541784c8bded9301aff0525
czcz7890 <dongsoochung99@gmail.com>
2019-07-12 12:55:17 -0400

    added edit event modal to front page

--- vendorTrainingSystem/src/app/components/front-page/front-page.component.ts
+++ vendorTrainingSystem/src/app/components/front-page/front-page.component.ts
@@ -3,6 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from '../../modals/register/register.component'
 import { LoginComponent } from '../../modals/login/login.component'
 import { ReportVendorComponent} from '../../modals/report-vendor/report-vendor.component'
+import { EventEditComponent} from '../../modals/event-edit/event-edit.component'
 
 @Component({
   selector: 'app-front-page',
@@ -13,6 +14,8 @@ export class FrontPageComponent implements OnInit {
   rc = RegisterComponent;
   lc = LoginComponent;
   reportVendorComponent = ReportVendorComponent;
+  eventEditComponent = EventEditComponent;
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
