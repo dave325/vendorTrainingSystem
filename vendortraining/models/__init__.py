e07f9c5331deceda8ba85fe1db2cfc73f87e0263
Kelvin Lui <kelvin97lui@gmail.com>
2019-07-18 17:40:36 -0400

    set up basic models & make migrations

---
+++ vendortraining/models/__init__.py
@@ -0,0 +1,6 @@
+from .attendance import Attendance
+from .event import Event
+from .member import Member
+from .role import Role
+from .user import User
+from .vendor import Vendor
--- vendortraining/settings.py
+++ vendortraining/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_mysql',
-    "rest_framework"
+    "rest_framework",
+    "vendortraining",
 ]
 
 MIDDLEWARE = [
