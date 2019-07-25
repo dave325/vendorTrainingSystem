3e1324fbb8d7fda7f26e8653926cbfa9de692493
David Dataram <davedataram@gmail.com>
2019-07-25 13:05:39 -0400

    Add user module and routes

--- vendortraining/urls.py
+++ vendortraining/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
-    path('admin/', admin.site.urls),   
+    path('users/', include('vendortraining.user.urls')),
+    path('admin/', admin.site.urls)
 ]
