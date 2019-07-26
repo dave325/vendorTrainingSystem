019d461a82ab35b0afe61e74a6de407c73b2cb03
David <davedataram@gmail.com>
2019-07-26 15:27:33 -0400

    final chnages for endpoint user

--- vendortraining/user/user.py
+++ vendortraining/user/user.py
@@ -8,6 +8,8 @@ from vendortraining.models import User
 from vendortraining.models.serializers import userSerializer
 from vendortraining.models import Event 
 from vendortraining.models.serializers import eventSerializer
+from vendortraining.models import Vendor 
+from vendortraining.models.serializers import vendorSerializer
 
 class UserView(viewsets.ModelViewSet):
     """
@@ -28,65 +30,61 @@ class UserView(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def listAllEvents(self, request, *args, **kwargs):
         queryset = Event.objects.all()
-        serializer_class = eventSerializer.EventSerializer(queryset)
+        serializer_class = eventSerializer.EventSerializer(queryset, many=true)
         return Response(serializer_class.data)
     
 #user seeing ONE event    
     @action(detail=False, methods=['get'])
     def listEvent(self, request, *args, **kwargs):
-        return Response("Helevent is listed")
-        queryset = User.objects.all()
+        queryset = queryset = Event.objects.filter(event_id = self.request.data.get('event_id'))
         serializer_class = userSerializer
+        return Response("event is listed")
     
-#user joining an event. dont know how to change/update database where the attedance would change    
+#user joining ONE event. dont know if we should include the change to the attendance table somehow   
     @action(detail=False, methods=['post'])
     def joinEvent(self, request, *args, **kwargs):
-      #  if 'eventid' in kwargs:
-       #     a = Attendance(event_id = kwargs.get('eventid'), )
-
-            queryset = User.objects.get(event_id = kwargs.get('eventid'))
+            queryset = Event.objects.filter(event_id = self.request.data.get('event_id'))
             serializer_class = eventSerializer.EventSerializer(queryset)
             return Response(serializer_class.data)
             
 #reporting an event 
-    """ @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'])
     def reportEvent(self, request, *args, **kwargs):
-        queryset = User.objects.get(event_id = input)
-        serializer_class = userSerializer
-        return Response("YOU ARE IN THE DOG HOUSE") """
+        queryset = Event.objects.filter(event_id = self.request.data.get('event_id'))
+        serializer_class = eventSerializer.EventSerializer(queryset)
+        return Response(serializer_class.data)
     
 #reporting a vendor    
-    """ @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'])
     def reportVendor(self, request, *args, **kwargs):
-        queryset = User.objects.get(event_id = input)
-        serializer_class = userSerializer
-        return Response("welcome to jail kind sir") """
+        queryset = Vendor.objects.filter(vendor_id = self.request.data.get('vendor_id'))
+        serializer_class = vendorSerializer.VendorSerializer(queryset)
+        return Response(serializer_class.data)
 
 #view user profile        
         @action(detail=False, methods=['post'])
         def profile(self, request, *args, **kwargs):
-        queryset = User.objects.all()
+        queryset = User.objects.filter(user_id = self.request.data.get('user_id'))
         serializer_class = userSerializer
         return Response(serializer_class.data)
 
 #deleting a user profile
         @action(detail=False, methods=['get'])
         def profileDelete(self, request, *args, **kwargs):
-        queryset = User.objects.all()
+        queryset = User.objects.filter(user_id = self.request.data.get('user_id'))
         serializer_class = userSerializer
         return Response(serializer_class.data)
-
 
 #editing a user profile
         @action(detail=False, methods=['get'])
         def profileEdit(self, request, *args, **kwargs):
-        queryset = User.objects.all()
+        queryset = User.objects.filter(user_id = self.request.data.get('user_id'))
         serializer_class = userSerializer
         return Response(serializer_class.data)
 
-#view events currently signed up for 
+#view events currently signed up for by the user
         @action(detail=False, methods=['post'])
-        def profileEvents(self, request, *args, **kwargs):
-        queryset = User.objects.all()
-        serializer_class = eventSerializer.EventSerializer(queryset)
+        def userEvents(self, request, *args, **kwargs):
+        queryset = Event.objects.filter(event_id = self.request.data.get('event_id'))
+        serializer_class = eventSerializer.EventSerializer(queryset,many = true)
         return Response(serializer_class.data)
