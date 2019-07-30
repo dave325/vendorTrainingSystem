ab78640dcf4adc41044f84ffe2491039bf80ceb3
David Dataram <davedataram@gmail.com>
2019-07-30 11:21:41 -0400

    Added methods listEvent and deleteevent in vendor and fixed user

--- vendortraining/models/serializers/userSerializer.py
+++ vendortraining/models/serializers/userSerializer.py
@@ -4,4 +4,4 @@ from vendortraining.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields =["id", "email", "first_name", "last_name", "role_id", "phone", "address", "public", "password"]
+        fields = '__all__'
