d932fe8ff78cc5bb4871f60222f54c3470cb8853
edwardcerverizzo1 <Edward.Cerverizzo@gmail.com>
2019-07-30 15:34:22 -0400

    finished editMyProfile in vendor.py

--- vendortraining/user/vendor.py
+++ vendortraining/user/vendor.py
@@ -26,20 +26,33 @@ class VendorView(viewsets.ModelViewSet):
         s = vendorSerializer.VendorSerializer(self.queryset, many=True)
         return Response(s.data)
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['get'])
     def getMyProfile(self, request, *args, **kwargs):
-        # qset = Vendor.objects.filter(vendor_id = self.request.data.get('vendor_id')) #self.request.data is a python dictionary. use get return the value
-        # s = vendorSerializer.VendorSerializer(qset, many=True)
-        # return Response(s.data)
-        return()
+        qset = Vendor.objects.filter(vendor_id = self.request.data.get('vendor_id')) #self.request.data is a python dictionary. use get return the value
+        s = vendorSerializer.VendorSerializer(qset, many=True)
+        return Response(s.data)
     
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['delete'])
     def deleteMyProfile(self, request, *args, **kwargs):
-        return()
+        vendorProfile = Vendor.objects.filter(vendor_id = self.request.data.get('vendor_id'))
+        vendorProfile.delete()
+        return("profile deleted")
 
     @action(detail=False, methods=['post'])
     def editMyProfile(self, request, *args, **kwargs):
-        return()
+        vendorProfile = Vendor.objects.filter(vendor_id = self.request.data.get('vendor_id'))
+        y = ""
+        d = {}
+        for x in self.request.data:
+            if len(str(self.request.data.get(x))) == 0: #If the data field is empty, we assume that we do not want to update that value----How it works:We convert to str to use len(). Probably a more elegent way of doing this though
+                continue
+            d[x] = self.request.data.get(x)
+            # x = str(self.request.data.get(x))
+            # y = y + " " + x
+
+        vendorProfile.update(**d)
+
+        return Response(y) # for debug-- not nessicary
 
     @action(detail=False, methods=['post'])
     def viewMyEvents(self, request, *args, **kwargs):
