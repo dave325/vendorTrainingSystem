5e3b0ddeb1c35db5cc8392f31ddd10a7ca26dee6
czcz7890 <dongsoochung99@gmail.com>
2019-08-01 13:29:22 -0400

    created connection to admin backend

--- vendortraining/user/urls.py
+++ vendortraining/user/urls.py
@@ -3,13 +3,14 @@ from rest_framework.routers import DefaultRouter
 from . import user
 from . import vendor
 from . import super
+from . import admin
 from django.urls import include, path
 
 router = DefaultRouter()
 router.register('user', user.UserView) #-- user profile route -- to access any function use "192.168.99.100:3000/user/<functionName>/" to access
 router.register('vendor', vendor.VendorView) #-- 192.168.99.100:3000/vendor/<functionName>/
 router.register('super', super.SuperView) #-- 192.168.99.100:3000/vendor/<functionName>/
-
+router.register('admin', admin.AdminView)
 
 
 # The API URLs are now determined automatically by the router.
