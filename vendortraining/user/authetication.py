441b6c278d7506e51e80efbec3d9528bbd7732c8
David <davedataram@gmail.com>
2019-08-01 13:45:38 -0400

    authetticaton change

--- vendortraining/user/authetication.py
+++ vendortraining/user/authetication.py
@@ -1,14 +1,23 @@
 from vendortraining.models import User
 from django.contrib.auth.models import User
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
-class UserAuthetication(viewsets.ModelViewSet):
-
-    def checkUser(user_id)
-        if user_id = self.request.data.get('user_id'):
+class UserAuthetication(viewsets.ViewSet):
+    
+    queryset = User.objects.all()
+    @action(detail=False, methods=['post'])
+    def checkUser(self, request, *args, **kwargs):
+        user = User.objects.get(id=self.request.data.get('user_id'))
+        if self.request.data.get('user_id') == "1ß":
             return True
-        elif user_id:
-            return False    
-
-    def checkId(user_id)
-        if 
+        else:
+            return False  
+    @action(detail=False, methods=['post'])
+    def checkId(self, request, *args, **kwargs):
+        if User.objects.filter(id == self.request.data.get('user_id')):
+            return True
+        else:
+            return False
