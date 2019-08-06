79b97f9913c75a4f6dbeaf150edadc3991da65c3
czcz7890 <dongsoochung99@gmail.com>
2019-08-06 14:33:22 -0400

    checking token key for authenticated user

--- vendortraining/urls.py
+++ vendortraining/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+
+from .views import login    #for token test
 urlpatterns = [
     path('', include('vendortraining.user.urls')),
     path('api-token-auth/', views.obtain_auth_token),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/login', login)    #testing token generation
 ]
