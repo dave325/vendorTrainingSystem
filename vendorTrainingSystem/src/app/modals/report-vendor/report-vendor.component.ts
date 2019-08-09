c4420a3d3793e084adf6b0ce09e5879b5091365b
David Dataram <davedataram@gmail.com>
2019-08-09 15:22:27 -0400

    testing userservice

--- vendorTrainingSystem/src/app/modals/report-vendor/report-vendor.component.ts
+++ vendorTrainingSystem/src/app/modals/report-vendor/report-vendor.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {UserService} from '../../user.service';
-
+import { User } from 'src/app/models/User';
+import { Vendor} from '../../models/Vendor';
 interface ReportVendorModel{
   vendorName: string;
   violation: string;
@@ -14,6 +15,9 @@ interface ReportVendorModel{
 })
 export class ReportVendorComponent implements OnInit {
   reportVendorModel = <ReportVendorModel>{};
+  vendor= {
+    name: 'name', address: 'address', phone: 'phone', email: 'email', is_approved: false
+  }
   info = null;
   error = null;
   constructor(activeModal: NgbActiveModal,
@@ -26,6 +30,14 @@ export class ReportVendorComponent implements OnInit {
   }
 
   onSubmitReport(){
+    this.userService.getUser(this.vendor).then(
+      (res) =>{
+        
+      },
+      (err)=>{
+
+      }
+    )
     let valid: boolean;
     alert("reported vendor: "+this.reportVendorModel.vendorName+", violation: "+this.reportVendorModel.violation);
     
