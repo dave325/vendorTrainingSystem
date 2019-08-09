ddcf920d76991645c933c918cf692d94ef238662
Karanvir Singh <ksin3798@gmail.com>
2019-08-09 15:18:03 -0400

    register component ts changes

--- vendorTrainingSystem/src/app/modals/register/register.component.ts
+++ vendorTrainingSystem/src/app/modals/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'dsol-register',
@@ -7,28 +8,39 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  firstName: string;
-  lastName: string;
-  phone1: number;
-  email1: string;
-  password1: string;
-  confirmPassword: string;
+  register = { 
+  firstName: <string> null,
+  lastName: <string> null,
+  phone1: <number> null,
+  email1: <string> null,
+  password1: <string> null,
+  confirmPassword: <string> null,
   //error messaging
-  errorMsgFirstName = "You must enter a valid First Name";
-  errorMsgLastName: "You must enter a valid Last Name";
-  errorMsgPhone: "You must enter a valid Phone Number";
-  errorMsgEmail: "You must enter a valid Email";
-  errorMsgPassword: "You must enter a valid Password";
-  errorMsgConfirmPassword: "Passwords do not match";
+  errorMsgFirstName: "You must enter a valid First Name",
+  errorMsgLastName: "You must enter a valid Last Name",
+  errorMsgPhone: "You must enter a valid Phone Number",
+  errorMsgEmail: "You must enter a valid Email",
+  errorMsgPassword: "You must enter a valid Password",
+  errorMsgConfirmPassword: "Passwords do not match",
+  }
 
-  constructor(public modalService: NgbActiveModal) { }
+  constructor(
+    public modalService: NgbActiveModal,
+    private userService: UserService,
+    ) { }
 
   ngOnInit() {
   }
   
-  //these two lines are temporary 
-  submitted = false;
-
-  ngSubmit() { this.submitted = true; }
-
+  onSubmitTemplateBased(){
+    
+    this.userService.getUser(this.register).then(
+      (res) =>{
+        console.log("success")
+      },
+      (err)=>{
+        console.log("error")
+      }
+    )
+  }
 }
