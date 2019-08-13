09f116dde77d222a3c5c3247dab6ba2a68e24db1
Karanvir Singh <ksin3798@gmail.com>
2019-08-13 13:06:07 -0400

    changes to delete and user edt modals

---
+++ vendorTrainingSystem/src/app/modals/delete-profile/delete-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dsol-delete-profile',
+  templateUrl: './delete-profile.component.html',
+  styleUrls: ['./delete-profile.component.css']
+})
+export class DeleteProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
