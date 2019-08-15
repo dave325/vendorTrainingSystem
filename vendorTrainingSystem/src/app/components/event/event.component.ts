324a70df4bd4f4f8b5df81a4d5b3b59f725ac37e
edwardcerverizzo1 <Edward.Cerverizzo@gmail.com>
2019-08-15 13:31:49 -0400

    edit-vendor-profile button working

--- vendorTrainingSystem/src/app/components/event/event.component.ts
+++ vendorTrainingSystem/src/app/components/event/event.component.ts
@@ -2,8 +2,6 @@ import { EventEditComponent } from './../../modals/event-edit/event-edit.compone
 import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { trigger, state, style, animate, transition, keyframes, sequence } from '@angular/animations';
 
-
-
 import { Event } from '../../models/Event';
 import { EventModalComponent } from 'src/app/modals/event-modal/event-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -31,19 +29,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
         opacity: 1,
         backgroundColor: 'green'
       })),
-      
+
       transition('open => closed', [
         animate('.5s')
       ]),
       transition('closed => open', [
         animate('10s'
           , keyframes([
-          // style({ width: "100%" , offset: .5}),
-          // style({ height: "100%", offset: .5}),
-          style({ backgroundColor: 'black', height: "1%", offset: 0}),
-          style({ backgroundColor: 'orange', height: "100%", offset: 0}),
-          style({ backgroundColor: 'red',  width: "100%", offset: 0.2}),
-          style({ backgroundColor: 'blue', width: "1%", offset: 1.0})])
+            // style({ width: "100%" , offset: .5}),
+            // style({ height: "100%", offset: .5}),
+            style({ backgroundColor: 'black', height: "1%", offset: 0 }),
+            style({ backgroundColor: 'orange', height: "100%", offset: 0 }),
+            style({ backgroundColor: 'red', width: "100%", offset: 0.2 }),
+            style({ backgroundColor: 'blue', width: "1%", offset: 1.0 })])
         )
       ]),
     ]),
@@ -52,12 +50,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 
-      export class EventComponent implements OnInit {
+export class EventComponent implements OnInit {
 
   isOpen = false;
   isOpenVertical = false;
   classListNames;
-  cn = "col-12 col-md-3";;
+  cn = "col-12 col-md-3";
   constructor(private modalService: NgbModal) {
     this.classListNames = this.cn;
   }
@@ -89,7 +87,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   ngOnInit() {
   }
 
-  editEvent(event){
+  editEvent(event) {
     const editModal = this.modalService.open(EventEditComponent);
     editModal.componentInstance.event = event;
     editModal.result.then((result) => {
