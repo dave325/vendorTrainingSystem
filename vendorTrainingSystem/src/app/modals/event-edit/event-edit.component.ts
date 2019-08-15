4c237acf9159773ff2eb997cd760f6d05884a543
czcz7890 <dongsoochung99@gmail.com>
2019-08-15 12:06:37 -0400

    pulling

--- vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
+++ vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AuthenticationService } from './../../services/Authentication.service';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Event } from '../../models/Event';
-//import { EventService} from '../../services/EventService';
+import { EventService} from './../../services/EventService';
 
 @Component({
   selector: 'dsol-event-edit',
@@ -10,18 +9,16 @@ import { Event } from '../../models/Event';
   styleUrls: ['./event-edit.component.css']
 })
 export class EventEditComponent implements OnInit {
-  @Input() event;
+  //@Input() event;
   constructor(
     public modalService: NgbModal, 
-    //private eventSerivce: EventService,
-    private auth: AuthenticationService) { 
-    console.log(this.event);
+    private eventSerivce: EventService) { 
   }
 
   ngOnInit() {
   }
 
-  submit(){
+  onSubmitTemplateBased(){
     
   }
 }
