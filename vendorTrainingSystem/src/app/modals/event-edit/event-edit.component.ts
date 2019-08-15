9264cdd77f96e6e72105f9b787c3f3a34192bd28
Karanvir Singh <ksin3798@gmail.com>
2019-08-15 12:08:17 -0400

    fixed merge conflict

--- vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
+++ vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-<<<<<<< HEAD
 import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap'
 import { HttpClient } from '@angular/common/http'
 import { Event } from '../../models/Event';
 import { UserService } from 'src/app/user.service'
-=======
-import { AuthenticationService } from './../../services/Authentication.service';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Event } from '../../models/Event';
-//import { EventService} from '../../services/EventService';
->>>>>>> fafa0ac22cd91069dad915ec89f25ad9816acca1
 
 @Component({
   selector: 'dsol-event-edit',
@@ -28,16 +21,10 @@ export class EventEditComponent implements OnInit {
 
   @Input() event;
   constructor(
-<<<<<<< HEAD
     activeModal: NgbActiveModal,
     private http: HttpClient,
     private userService: UserService,
     ) { 
-=======
-    public modalService: NgbModal, 
-    //private eventSerivce: EventService,
-    private auth: AuthenticationService) { 
->>>>>>> fafa0ac22cd91069dad915ec89f25ad9816acca1
     console.log(this.event);
   }
 
