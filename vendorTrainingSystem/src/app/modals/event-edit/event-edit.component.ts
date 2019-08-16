ad2c8a74f6826a1459190f868f4e1048ccf70ae7
Karanvir Singh <ksin3798@gmail.com>
2019-08-16 12:20:33 -0400

    minor changes

--- vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
+++ vendorTrainingSystem/src/app/modals/event-edit/event-edit.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http'
 import { Event } from '../../models/Event';
@@ -14,6 +13,7 @@ export class EventEditComponent implements OnInit {
   @Input() event;
 
   constructor(
+    private http: HttpClient,
     activeModal: NgbActiveModal,
     private eventService: EventService
   ) {
