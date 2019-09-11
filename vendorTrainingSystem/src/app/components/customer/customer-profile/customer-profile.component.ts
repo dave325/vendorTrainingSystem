e26e398751f68cb0f2d0ecd4daa8dca33bf51b68
czcz7890 <dongsoochung99@gmail.com>
2019-09-10 21:04:53 -0400

    connecting backend from customer profile

--- vendorTrainingSystem/src/app/components/customer/customer-profile/customer-profile.component.ts
+++ vendorTrainingSystem/src/app/components/customer/customer-profile/customer-profile.component.ts
@@ -4,13 +4,14 @@ import { dummy_events } from '../../../dummy-data/dummy-events';
 import { UserEditComponent } from './../../../modals/user-edit/user-edit.component';
 import { DeleteProfileComponent } from './../../../modals/delete-profile/delete-profile.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from 'src/app/user.service';
 @Component({
   selector: 'app-customer-profile',
   templateUrl: './customer-profile.component.html',
   styleUrls: ['./customer-profile.component.css']
 })
 export class CustomerProfileComponent implements OnInit {
-
+  private customer;
   Events = dummy_events;
   user_info = {
     username: "samsmith123",
@@ -20,7 +21,11 @@ export class CustomerProfileComponent implements OnInit {
     email: "spring123@gmail.com",
     address: "123 York Ave."
   }
-  constructor(private modalService:NgbModal) { }
+  constructor(private modalService:NgbModal,
+    private userService: UserService
+    ) {
+    this.customer = UserService.getUser(); 
+  }
 
   ngOnInit() {
   }
